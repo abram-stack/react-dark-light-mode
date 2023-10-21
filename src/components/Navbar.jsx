57d4511fc6e30da85bf45cc4e423779d837d958c
abram-stack <anthonybramantyo@outlook.com>
2023-10-21 20:06:32 +0200

    changing state and UI, toggler slider

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 
-export default function Navbar() {
-  function handleToggle() {
-    console.log('toggle')
-  }
+export default function Navbar(props){
+  
+
+  let classDark = props.darkMode ? 'dark' : ''
+  console.log(classDark)
+
   return (
     <>
-      <nav className='navbar container'>
+      <nav className={`navbar container ${classDark}`}>
         <div className='logo-container'>
           <img src='./react-icon-small.png' className='logo-image'/>
         </div>
         <div className='toggler'>
           <p className='toggler--light'>light</p>
           <div className='toggler--slider'
-            onClick={handleToggle}>
+            onClick={() => props.toggleDark()}>
             <div className='toggler--slider-circle'></div>
           </div>
           <p className='toggler--dark'>dark</p>
